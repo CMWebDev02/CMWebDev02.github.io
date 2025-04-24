7a1bb94f380f7dac989f48027c691dcef998c822
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2025-04-24 09:44:32 -0700

    Began working on the navigation bar and added the main image assets to the project

---
+++ src/components/ui/navbar.tsx
@@ -0,0 +1,13 @@
+export function NavBar() {
+    return (
+        <nav>
+            <div>
+          <a>
+            <img id="website-logo" src="./assets/CMWebDev.svg" alt="CMWebDev Website Logo" />
+          </a>
+        
+        <button>Menu</button>
+        </div>
+        </nav>
+    )
+}
