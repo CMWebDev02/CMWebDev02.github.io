0350b3eb51f6890a9329f74d5541276dac95e16b
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2025-06-10 18:39:57 -0700

    Moved the sample portfolio container into separate container, and removed the card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@
 // Sample Project Divs
 // Footer with contact and excess info about me.
 
-import CardContainer from "@/components/client/card-container";
 import HeroContainer from "@/containers/main-page/hero-container";
 import IntroContainer from "@/containers/main-page/intro-container";
+import PortfolioContainer from "@/containers/main-page/portfolio-container";
 
 export default function Home() {
   return (
@@ -15,18 +15,7 @@ export default function Home() {
 
       <IntroContainer />
 
-      <CardContainer
-        title="My Portfolio"
-        description="Some of my personal favorite projects."
-        links={[{ linkName: "Portfolio", href: { href: "/" } }]}
-      >
-        <div>
-          <h2>Project One</h2>
-        </div>
-        <div>
-          <h2>Project Two</h2>
-        </div>
-      </CardContainer>
+      <PortfolioContainer />
     </>
   );
 }
---
+++ src/containers/main-page/portfolio-container.tsx
@@ -0,0 +1,28 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../../components/ui/card";
+
+export default function PortfolioContainer() {
+  return (
+    <Card className="container w-11/12 sm:w-5/6 lg:w-3/4">
+      <CardHeader>
+        <CardTitle>My Portfolio</CardTitle>
+        <CardDescription>
+          Some of my personal favorite projects.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div>
+          <h2>Project One</h2>
+        </div>
+        <div>
+          <h2>Project Two</h2>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
