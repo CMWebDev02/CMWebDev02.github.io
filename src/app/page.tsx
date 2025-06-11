5b632d8e7a3b9528cd6bbaeca90ffca4b13a1f13
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2025-06-10 18:37:00 -0700

    Moved introduction container into own component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,30 +6,14 @@
 
 import CardContainer from "@/components/client/card-container";
 import HeroContainer from "@/containers/main-page/hero-container";
+import IntroContainer from "@/containers/main-page/intro-container";
 
 export default function Home() {
   return (
     <>    
       <HeroContainer />
-      
 
-      <CardContainer
-        title="Introduction:"
-        description="Allow me to introduce myself..."
-        links={[
-          { linkName: "About", href: { href: "/" } },
-          { linkName: "Contact", href: { href: "/" } },
-        ]}
-      >
-        <p>
-          I am Cristiano Miranda, and I currently pursuing a Bachelor Degree in
-          Computer Science and in my free time I work on personal and volunteer
-          projects. I have well over 2 years of experience with HTML, CSS, and
-          JavaScript. Along with almost 1 year of experience with React. As of
-          now, I am currently working with NextJs and NodeJs and so that I
-          continue working towards a career as a fullstack engineer.
-        </p>
-      </CardContainer>
+      <IntroContainer />
 
       <CardContainer
         title="My Portfolio"
