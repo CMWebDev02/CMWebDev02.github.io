6f022e93bcf6fbe9eb125d93f2edbe2b19077503
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2025-07-02 13:58:13 -0700

    Fixed and updated the navbar

--- src/containers/main-layout/navbar.tsx
+++ src/containers/main-layout/navbar.tsx
@@ -11,21 +11,21 @@ import { NavLink } from "@/components/client/nav-link";
 export function NavBar() {
   return (
     <header
-      className={`h-12 md:h-screen w-full md:w-12 group transition-[width] duration-400 ease-in-out hover:w-30 overflow-x-hidden bg-blue-200`}
+      className={`h-12 md:h-screen w-full md:w-12 group md:transition-[width] duration-400 ease-in-out md:hover:w-30 overflow-x-hidden bg-blue-200`}
     >
       {/* Mobile View NavBar */}
-      <nav className="flex md:hidden h-full w-full gap-2">
+      <nav className="flex md:flex-col align-middle items-center p-1 h-full w-full gap-2">
         <Image
           id="miniature-website-logo"
           src="./assets/CMWebDev-Graphic.svg"
           alt="CMWebDev Website Logo"
           width={150}
           height={150}
-          className="h-10 w-10 self-baseline"
+          className="h-10 w-10 md:w-full md:h-auto self-baseline"
         />
 
         <div
-          className={`h-full flex flex-row justify-around overflow-x-scroll gap-2`}
+          className={`h-full md:h-1/4 flex flex-row md:flex-col justify-around overflow-x-scroll md:overflow-x-hidden gap-2 md:sticky md:top-40`}
         >
           <NavLink href="./" linkName="Home" icon={faHouse} />
           <NavLink href="./about" linkName="About" icon={faUser} />
@@ -43,34 +43,6 @@ export function NavBar() {
         </div>
       </nav>
       {/* Desktop View NavBar */}
-      <nav
-        className={`hidden md:flex flex-col align-middle items-center p-1 h-full w-full`}
-      >
-        <Image
-          id="miniature-website-logo"
-          src="./assets/CMWebDev-Graphic.svg"
-          alt="CMWebDev Website Logo"
-          width={150}
-          height={150}
-          className="w-full self-baseline"
-        />
-
-        <div className={`h-1/4 flex flex-col justify-around sticky top-40`}>
-          <NavLink href="./" linkName="Home" icon={faHouse} />
-          <NavLink href="./about" linkName="About" icon={faUser} />
-          <NavLink
-            href="./portfolio"
-            linkName="Portfolio"
-            icon={faFolderTree}
-          />
-          <NavLink
-            href="./credentials"
-            linkName="Credentials"
-            icon={faIdCard}
-          />
-          <NavLink href="./contact" linkName="Contact" icon={faAddressBook} />
-        </div>
-      </nav>
     </header>
   );
 }
