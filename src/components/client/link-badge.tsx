e15d1e26a48127cb42f88b168c8fb66e752098fd
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2025-07-02 15:17:50 -0700

    Updated more colors

--- src/components/client/link-badge.tsx
+++ src/components/client/link-badge.tsx
@@ -17,10 +17,10 @@ export default function LinkBadge({
       className="h-full flex flex-row group cursor-pointer select-none"
       onClick={redirectFunction}
     >
-      <h3 className="text-4xl transition-[color] duration-200 ease-in-out group-hover:text-blue-600">
+      <h3 className="text-4xl transition-[color] duration-200 ease-in-out group-hover:text-highlight">
         {linkText}
       </h3>
-      <span className="text-xs transition-[color] duration-200 ease-in-out group-hover:text-green-500">
+      <span className="text-xs transition-[color] duration-200 ease-in-out group-hover:text-highlight">
         {children}
       </span>
     </Button>
