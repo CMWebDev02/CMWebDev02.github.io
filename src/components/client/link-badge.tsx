9388526f10f6b89c3f5c53e156ba4954588e73ee
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2025-07-02 15:48:31 -0700

    Added a portfolio link to the portfolio container and moved the button to a footer

--- src/components/client/link-badge.tsx
+++ src/components/client/link-badge.tsx
@@ -14,13 +14,13 @@ export default function LinkBadge({
 }: LinkBadgeProps) {
   return (
     <Button
-      className="h-full flex flex-row group cursor-pointer select-none"
+      className="h-full flex flex-row"
       onClick={redirectFunction}
     >
-      <h3 className="text-4xl transition-[color] duration-200 ease-in-out group-hover:text-highlight">
+      <h3 className="text-4xl">
         {linkText}
       </h3>
-      <span className="text-xs transition-[color] duration-200 ease-in-out group-hover:text-highlight">
+      <span className="text-xs">
         {children}
       </span>
     </Button>
--- src/containers/main-page/portfolio-container.tsx
+++ src/containers/main-page/portfolio-container.tsx
@@ -1,27 +1,31 @@
 import {
   Card,
   CardContent,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "../../components/ui/card";
 import ProjectCarousel from "@/components/client/project-carousel";
 import ProjectList from "@/components/client/project-list";
+import { Button } from "@/components/ui/button";
 import { projectsArray } from "@/lib/utils";
+import Link from "next/link";
 
 export default function PortfolioContainer() {
-
   return (
     <Card className="container w-full h-min bg-secondary-background">
       <CardHeader className="h-1/12">
-        <CardTitle>
-          <h2>Portfolio</h2>
-          <h3>Some of my personal favorite projects.</h3>
-        </CardTitle>
+        <CardTitle className="">Portfolio Project Highlights</CardTitle>
       </CardHeader>
       <CardContent className="h-11/12 w-full p-3 flex justify-center">
         <ProjectCarousel projectsArray={projectsArray} />
         <ProjectList projectsArray={projectsArray} />
       </CardContent>
+      <CardFooter className="w-full flex justify-center">
+        <Link href={"./portfolio"}>
+          <Button className="text-2xl">View More</Button>
+        </Link>
+      </CardFooter>
     </Card>
   );
 }
