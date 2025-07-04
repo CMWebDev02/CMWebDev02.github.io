187b1363c9879d18f74ed63fb5b95191be22c5a0
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2025-07-03 20:15:56 -0700

    Created the basic accordian layout and styled the portfolio container

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -4,55 +4,51 @@ import {
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "@/components/ui/accordion"
+} from "@/components/ui/accordion";
 import { projectsArray } from "@/lib/utils";
 import ProjectCarousel from "@/components/client/project-carousel";
 import ProjectList from "@/components/client/project-list";
 
 export default function PortfolioPage() {
-    
-    return (
-        <>
-            <Card className="w-full h-fit">
-                <CardHeader>
-                    <CardTitle>Portfolio Page</CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <Accordion type="single" collapsible>
-                        <AccordionItem  value="React">
-                            <AccordionTrigger>React</AccordionTrigger>
-                            <AccordionContent>
-                                <ProjectCarousel projectsArray={projectsArray} />
-                                <ProjectList projectsArray={projectsArray} />
-                            </AccordionContent>
-                        </AccordionItem>
-                    </Accordion>
-                    <Accordion type="single" collapsible>
-                        <AccordionItem  value="JavaScript">
-                            <AccordionTrigger>JavaScript</AccordionTrigger>
-                            <AccordionContent>
-
-                            </AccordionContent>
-                        </AccordionItem>
-                    </Accordion>
-                    <Accordion type="single" collapsible>
-                        <AccordionItem  value="HTML">
-                            <AccordionTrigger>HTML</AccordionTrigger>
-                            <AccordionContent>
-
-                            </AccordionContent>
-                        </AccordionItem>
-                    </Accordion>
-                    <Accordion type="single" collapsible>
-                        <AccordionItem  value="CMS">
-                            <AccordionTrigger>CMS</AccordionTrigger>
-                            <AccordionContent>
-
-                            </AccordionContent>
-                        </AccordionItem>
-                    </Accordion>
-                </CardContent>
-            </Card>
-        </>
-    )
+  return (
+    <>
+      <Card className="w-full lg:w-11/12 xl:w-4/5 h-full md:h-11/12 max-h-full">
+        <CardHeader>
+          <CardTitle>Portfolio</CardTitle>
+        </CardHeader>
+        <CardContent className="h-full max-h-full overflow-scroll">
+          <Accordion type="single" collapsible>
+            <AccordionItem value="React">
+              <AccordionTrigger>React</AccordionTrigger>
+              <AccordionContent>
+                <ProjectCarousel projectsArray={projectsArray} />
+                <ProjectList projectsArray={projectsArray} />
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="JavaScript">
+              <AccordionTrigger>JavaScript</AccordionTrigger>
+              <AccordionContent>
+                <ProjectCarousel projectsArray={projectsArray} />
+                <ProjectList projectsArray={projectsArray} />
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="HTML">
+              <AccordionTrigger>HTML</AccordionTrigger>
+              <AccordionContent>
+                <ProjectCarousel projectsArray={projectsArray} />
+                <ProjectList projectsArray={projectsArray} />
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="CMS">
+              <AccordionTrigger>CMS</AccordionTrigger>
+              <AccordionContent>
+                <ProjectCarousel projectsArray={projectsArray} />
+                <ProjectList projectsArray={projectsArray} />
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </CardContent>
+      </Card>
+    </>
+  );
 }
