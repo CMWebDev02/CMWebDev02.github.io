d14f169294b30dab6cdeb44bfb8c2e73b3a19eb4
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2025-07-03 19:55:11 -0700

    Added the main accordian groups and the project carousels and lists

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,7 +1,58 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import { projectsArray } from "@/lib/utils";
+import ProjectCarousel from "@/components/client/project-carousel";
+import ProjectList from "@/components/client/project-list";
+
 export default function PortfolioPage() {
     
     return (
         <>
+            <Card className="w-full h-fit">
+                <CardHeader>
+                    <CardTitle>Portfolio Page</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <Accordion type="single" collapsible>
+                        <AccordionItem  value="React">
+                            <AccordionTrigger>React</AccordionTrigger>
+                            <AccordionContent>
+                                <ProjectCarousel projectsArray={projectsArray} />
+                                <ProjectList projectsArray={projectsArray} />
+                            </AccordionContent>
+                        </AccordionItem>
+                    </Accordion>
+                    <Accordion type="single" collapsible>
+                        <AccordionItem  value="JavaScript">
+                            <AccordionTrigger>JavaScript</AccordionTrigger>
+                            <AccordionContent>
+
+                            </AccordionContent>
+                        </AccordionItem>
+                    </Accordion>
+                    <Accordion type="single" collapsible>
+                        <AccordionItem  value="HTML">
+                            <AccordionTrigger>HTML</AccordionTrigger>
+                            <AccordionContent>
+
+                            </AccordionContent>
+                        </AccordionItem>
+                    </Accordion>
+                    <Accordion type="single" collapsible>
+                        <AccordionItem  value="CMS">
+                            <AccordionTrigger>CMS</AccordionTrigger>
+                            <AccordionContent>
+
+                            </AccordionContent>
+                        </AccordionItem>
+                    </Accordion>
+                </CardContent>
+            </Card>
         </>
     )
 }
