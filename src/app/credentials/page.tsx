30dd561eeb67a000ca9fe322e9303bbc44703835
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2025-07-11 12:49:58 -0700

    Created the basis for displaying the certifications and degrees in an accordian

--- src/app/credentials/page.tsx
+++ src/app/credentials/page.tsx
@@ -1,17 +1,38 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Accordion } from "@radix-ui/react-accordion";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 export default function CredentialsPage() {
-  const CertificationItems = ["Bay Valley Tech", "Modesto Junior College", "Test Out", "Adobe", "CompTIA"];
+  const CredentialItems = [
+    "Bay Valley Tech",
+    "Modesto Junior College",
+    "CompTIA",
+    "Test Out",
+    "Adobe",
+  ].map((institute) => {
+    const filteredCredential = [];
+    return (
+      <AccordionItem value={institute} key={institute}>
+        <AccordionTrigger>{institute}</AccordionTrigger>
+        <AccordionContent className="w-full flex justify-center items-center"></AccordionContent>
+      </AccordionItem>
+    );
+  });
 
   return (
     <Card className="w-full lg:w-11/12 xl:w-4/5 h-full md:h-11/12 max-h-full md:max-h-11/12 overflow-scroll">
       <CardHeader>
-        <CardTitle className="text-md md:text-3xl">Certifications / Degrees</CardTitle>
+        <CardTitle className="text-md md:text-3xl">
+          Certifications / Degrees
+        </CardTitle>
       </CardHeader>
       <CardContent className="h-full">
         <Accordion type="single" collapsible>
-          {CertificationItems}
+          {CredentialItems}
         </Accordion>
       </CardContent>
     </Card>
