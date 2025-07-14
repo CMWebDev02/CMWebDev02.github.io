d3f8cc86c188ab2e8e137ca7d39829c4ee47fe48
CMWebDev02 <CMWebDev02@gmail.com>
2025-07-13 19:30:56 -0700

    Switched to a tab container from the accordian

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,46 +1,41 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
 import { projectsArray } from "@/lib/utils";
 import ProjectDisplay from "@/containers/projects/project-display";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function PortfolioPage() {
-  const ProjectItems = [
-    "Electron",
-    "React",
-    "Node JS",
-    "JavaScript",
-    "HTML",
-  ].map((type) => {
+  const projectTypes = ["Electron", "React", "Node JS", "JavaScript", "HTML"];
+
+  const ProjectTabTriggers = projectTypes.map((type) => {
+    return (
+      <TabsTrigger value={type} key={`${type}-trigger`}>
+        {type}
+      </TabsTrigger>
+    );
+  });
+
+  const ProjectTabContents = projectTypes.map((type) => {
     const filteredProjects = projectsArray.filter(
       (project) => project.projectType === type
     );
     return (
-      <AccordionItem value={type} key={type}>
-        <AccordionTrigger>{type}</AccordionTrigger>
-        <AccordionContent className="w-full flex justify-center items-center">
-          <ProjectDisplay projectsArray={filteredProjects} />
-        </AccordionContent>
-      </AccordionItem>
+      <TabsContent value={type} key={`${type}-content`} className="w-full">
+        <ProjectDisplay projectsArray={filteredProjects} />
+      </TabsContent>
     );
   });
 
   return (
-    <>
-      <Card className="w-full lg:w-11/12 xl:w-4/5 h-full md:h-11/12 max-h-full md:max-h-11/12 overflow-scroll">
-        <CardHeader>
-          <CardTitle className="text-md md:text-3xl">Portfolio</CardTitle>
-        </CardHeader>
-        <CardContent className="h-full">
-          <Accordion type="single" collapsible>
-            {ProjectItems}
-          </Accordion>
-        </CardContent>
-      </Card>
-    </>
+    <Card className="w-full lg:w-11/12 xl:w-4/5 h-full md:h-11/12 max-h-full md:max-h-11/12 overflow-scroll p-0">
+      <CardHeader className="h-1/12 p-1">
+        <CardTitle className="text-md md:text-3xl">Portfolio</CardTitle>
+      </CardHeader>
+      <CardContent className="h-11/12 max-h-11/12 overflow-y-auto w-full p-0">
+        <Tabs>
+          <TabsList>{ProjectTabTriggers}</TabsList>
+          {ProjectTabContents}
+        </Tabs>
+      </CardContent>
+    </Card>
   );
 }
